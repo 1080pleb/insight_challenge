282ac52daed429ec7dffe7c28680f08bf5b9450e
1080pleb <1080pleb@gmail.com>
2017-04-05 17:20:34 -0400

    further testing

--- src/bad_login_feature_extractor_test.py
+++ src/bad_login_feature_extractor_test.py
@@ -53,6 +53,36 @@ class TestBadLoginFeatureExtractor(unittest.TestCase):
         'f - - [02/Jul/1995:00:31:13 -0400] "GET /login HTTP/1.0" 200 0',
         'f - - [02/Jul/1995:00:31:14 -0400] "POST /login HTTP/1.0" 401 0',
     ])
+
+  def test_diff_hosts(self):
+    records = [
+        record.Record('f - - [01/Jul/1995:00:00:01 -0400] "POST /login HTTP/1.0" 401 0'),
+        record.Record('f - - [01/Jul/1995:00:00:15 -0400] "POST /login HTTP/1.0" 401 0'),
+        record.Record('e - - [01/Jul/1995:00:00:17 -0400] "POST /login HTTP/1.0" 401 0'), 
+        record.Record('f - - [01/Jul/1995:00:00:21 -0400] "POST /login HTTP/1.0" 401 0'),
+        record.Record('f - - [01/Jul/1995:00:00:26 -0400] "POST /login HTTP/1.0" 401 0'),
+        record.Record('f - - [01/Jul/1995:00:01:13 -0400] "GET /login HTTP/1.0" 401 0'),
+        record.Record('e - - [01/Jul/1995:00:04:14 -0400] "POST /login HTTP/1.0" 402 0'),
+        record.Record('f - - [02/Jul/1995:00:00:18 -0400] "POST /login HTTP/1.0" 401 0'),
+        record.Record('f - - [02/Jul/1995:00:31:11 -0400] "POST /login HTTP/1.0" 401 0'),
+        record.Record('f - - [02/Jul/1995:00:31:11 -0400] "POST /login HTTP/1.0" 401 0'),
+        record.Record('f - - [02/Jul/1995:00:31:11 -0400] "POST /login HTTP/1.0" 401 0'),
+        record.Record('f - - [02/Jul/1995:00:31:11 -0400] "POST /login HTTP/1.0" 401 0'),
+        record.Record('f - - [02/Jul/1995:00:31:12 -0400] "POST /login HTTP/1.0" 401 0'),
+        record.Record('a - - [02/Jul/1995:00:31:13 -0400] "GET /login HTTP/1.0" 200 0'),
+        record.Record('f - - [02/Jul/1995:00:31:14 -0400] "POST /login HTTP/1.0" 401 0'),
+    ]
     
+    h = BadLoginFeatureExtractor()
+    for r in records:
+      h.add_record(r)
+    results = h.flush()
+    self.assertListEqual(results, [
+        'f - - [01/Jul/1995:00:01:13 -0400] "GET /login HTTP/1.0" 401 0',
+        'f - - [02/Jul/1995:00:31:11 -0400] "POST /login HTTP/1.0" 401 0',
+        'f - - [02/Jul/1995:00:31:12 -0400] "POST /login HTTP/1.0" 401 0',
+        'f - - [02/Jul/1995:00:31:14 -0400] "POST /login HTTP/1.0" 401 0',
+    ])
+        
 if __name__ == '__main__':
   unittest.main()
