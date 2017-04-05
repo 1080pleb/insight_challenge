057c03b70cb8ed39bb24e2cca9a43fcf29d5959f
1080pleb <1080pleb@gmail.com>
2017-04-05 10:41:15 -0400

    added new files derp.

---
+++ src/hosts_feature_extractor.py
@@ -0,0 +1,17 @@
+from record import Record
+from collections import defaultdict
+import heapq
+
+class HostsFeatureExtractor:
+  def __init__(self):
+    self.host_list = defaultdict(lambda: 0)
+    
+  def add_record(self, r):
+    """Adds a record to the feature extractor"""
+    self.host_list[r.hostname] += 1
+ 
+  def flush(self):
+    return self.host_list
+    # dump 10 highest
+    #top_10_hosts = heapq.nlargest(10, hostList)
+    
