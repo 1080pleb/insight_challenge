4a24fc162ab4e05156678e04fca40547e90ebd3a
Matt Kulukundis <matt.fowles@gmail.com>
2017-04-05 10:27:04 -0400

    add a basic test for hosts feature extractor

---
+++ src/hosts_feature_extractor_test.py
@@ -0,0 +1,19 @@
+import unittest
+import record
+from hosts_feature_extractor import HostsFeatureExtractor
+
+class TestHostsFeatureExtractor(unittest.TestCase):
+  def test_provided_records_file(self):
+    records = record.read_from_file('../insight_testsuite/tests/test_features/log_input/log.txt')
+    h = HostsFeatureExtractor()
+    for r in records:
+      h.add_record(r)
+    results = r.flush()
+    self.assertDictEqual(results, {
+        "199.72.81.55": 6,
+        "burger.letters.com": 3,
+        "unicomp6.unicomp.net": 1
+    })
+
+if __name__ == '__main__':
+  unittest.main()
