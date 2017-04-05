b0e1511d8b779e580506379ea793fbabc6c140bd
1080pleb <1080pleb@gmail.com>
2017-04-05 14:58:59 -0400

    further fixed test, time window complete FOR NOW  Please enter the commit message for your changes. Lines starting

--- src/busy_hours_feature_extractor_test.py
+++ src/busy_hours_feature_extractor_test.py
@@ -22,5 +22,51 @@ class TestBusyHoursFeatureExtractor(unittest.TestCase):
         (record.parse_time('01/Jul/1995:00:00:10 -0400'), 7),
     ])
 
+  def test_larger_records_file(self):
+    records = record.read_from_file('../insight_testsuite/tests/test_features/log_input/log_test_100.txt')
+    h = BusyHoursFeatureExtractor()
+    for r in records:
+      h.add_record(r)
+    results = h.flush()
+    self.assertListEqual(results, [
+        (record.parse_time('01/Jul/1995:00:00:01 -0400'), 100),
+        (record.parse_time('01/Jul/1995:00:00:02 -0400'), 99),
+        (record.parse_time('01/Jul/1995:00:00:03 -0400'), 99),
+        (record.parse_time('01/Jul/1995:00:00:04 -0400'), 99),
+        (record.parse_time('01/Jul/1995:00:00:05 -0400'), 99),
+        (record.parse_time('01/Jul/1995:00:00:06 -0400'), 99),
+        (record.parse_time('01/Jul/1995:00:00:07 -0400'), 98),
+        (record.parse_time('01/Jul/1995:00:00:08 -0400'), 98),
+        (record.parse_time('01/Jul/1995:00:00:09 -0400'), 98),
+        (record.parse_time('01/Jul/1995:00:00:10 -0400'), 93),
+    ])
+    
+  def test_window_closing(self):
+    records = [
+        record.Record('b - - [01/Jul/1995:00:00:11 -0400] "GET /b HTTP/1.0" 304 0'),
+        record.Record('c - - [01/Jul/1995:00:00:12 -0400] "GET /c HTTP/1.0" 304 0'),
+        record.Record('a - - [01/Jul/1995:23:43:11 -0400] "GET /a HTTP/1.0" 304 0'),
+        record.Record('d - - [02/Jul/1995:00:00:12 -0400] "GET /b HTTP/1.0" 304 0'),
+        record.Record('e - - [02/Jul/1995:00:31:11 -0400] "GET /c HTTP/1.0" 304 0'),
+        record.Record('f - - [04/Jul/1995:00:00:11 -0400] "GET /a HTTP/1.0" 304 0'),
+    ]
+
+    h = BusyHoursFeatureExtractor()
+    for r in records:
+      h.add_record(r)
+    results = h.flush()
+    self.assertListEqual(results, [
+        (record.parse_time('01/Jul/1995:23:31:12 -0400'), 3),
+        (record.parse_time('01/Jul/1995:23:31:13 -0400'), 3),
+        (record.parse_time('01/Jul/1995:23:31:14 -0400'), 3),
+        (record.parse_time('01/Jul/1995:23:31:15 -0400'), 3),
+        (record.parse_time('01/Jul/1995:23:31:16 -0400'), 3),
+        (record.parse_time('01/Jul/1995:23:31:17 -0400'), 3),
+        (record.parse_time('01/Jul/1995:23:31:18 -0400'), 3),
+        (record.parse_time('01/Jul/1995:23:31:19 -0400'), 3),
+        (record.parse_time('01/Jul/1995:23:31:20 -0400'), 3),
+        (record.parse_time('01/Jul/1995:23:31:21 -0400'), 3),
+    ])    
+    
 if __name__ == '__main__':
   unittest.main()
